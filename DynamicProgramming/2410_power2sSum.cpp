8005a85f5a1e9fb61ad54470e798d17a0bfe44f4
tlsgkdns <tlsgkdns@gmail.com>
2021-03-13 18:43:33 +0900

    new files

---
+++ DynamicProgramming/2410_power2sSum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int n;
+int dp[1000001][21];
+
+int solve(int num, int prev)
+{
+	if (num < 0) return 0;
+	if (num == 0) return 1;
+	int& ret = dp[num][prev];
+	if (ret > 0) return ret % 1000000000;
+	for (int i = prev; i >= 0; i--)
+	{
+		ret += solve(num - pow(2, i), i) % 1000000000;
+		ret %= 1000000000;
+	}
+	return ret % 1000000000;
+}
+int main()
+{
+	cin >> n;
+	cout << solve(n, 20) << '\n';
+}
+
+// https://www.acmicpc.net/problem/2410
+
+/*
+Harder than I expected, But, not that difficult.
+*/
---
+++ Math/Binary_Search/7795_eatOrBeAte.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> ans;
+int t, n, m;
+int main()
+{
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		int s = 0;
+		cin >> n >> m;
+		vector<int> A(n), B(m);
+		for (int j = 0; j < n; j++) cin >> A[j];
+		for (int j = 0; j < m; j++) cin >> B[j];
+		sort(B.begin(), B.end());
+		for (int j = 0; j < n; j++)
+		{
+			int left = 0, right = m - 1;
+			auto it = lower_bound(B.begin(), B.end(), A[j]);
+			s += it - B.begin();
+		}
+		ans.push_back(s);
+	}
+	for (int a : ans)	cout << a << '\n';
+}
+
+// https://www.acmicpc.net/problem/7795
+/*
+sort and binary search.
+*/
