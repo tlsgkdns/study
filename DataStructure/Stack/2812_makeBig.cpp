f4770ea074e5fa9212c8b68d200bbeb093303899
tlsgkdns <tlsgkdns@gmail.com>
2021-03-14 17:38:23 +0900

    new files

---
+++ DataStructure/Stack/2812_makeBig.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+int n, k, cnt = 0;
+string s, ans = "";
+stack<int> st;
+int main()
+{
+	cin >> n >> k;
+	cin >> s;
+	for (int i = 0; i < n; i++)
+	{
+		if (cnt == k)
+		{
+			st.push(s[i]);
+			continue;
+		}
+		if (st.empty())
+			st.push(s[i]);
+		else
+		{
+			while (!st.empty() && st.top() < s[i])
+			{
+				st.pop();
+				cnt++;
+				if (cnt == k) break;
+			}
+			st.push(s[i]);
+		}
+	}
+	while (cnt != k)
+	{
+		cnt++;
+		st.pop();
+	}
+	while (!st.empty())
+	{
+		ans.push_back(st.top());
+		st.pop();
+	}
+	reverse(ans.begin(), ans.end());
+	cout << ans << '\n';
+}
+
+// https://www.acmicpc.net/problem/2812
+
+/*
+stack + greedy.
+*/
---
+++ Math/11689_GCD(n,k)=1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using ll = long long;
+using namespace std;
+
+int main()
+{
+	ll n, p;
+	cin >> n;
+	p = n;
+	for (ll i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			p /= i;
+			p *= (i - 1);
+		}
+		while (n % i == 0) n /= i;
+	}
+	if (n != 1)
+	{
+		p /= n;
+		p *= (n - 1);
+	}
+	cout << p << '\n';
+}
+
+// https://www.acmicpc.net/problem/11689
+
+/*
+Leared Euler's phi function.
+I have to understand the code more.
+*/
