f997c4f0413ae1b4adb49263e5e49ec818e38fb7
tlsgkdns <tlsgkdns@gmail.com>
2021-10-12 20:32:41 +0900

    Standard KMP problem

---
+++ String/KMP/1786_find.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> getPi(string p)
+{
+	int n = p.size();
+	int j = 0;
+	vector<int> pi(n, 0);
+	for (int i = 1; i < n; i++)
+	{
+		while (j > 0 && p[i] != p[j])
+			j = pi[j - 1];
+		if (p[i] == p[j])
+			pi[i] = ++j;
+	}
+	return pi;
+}
+
+vector<int> kmp(string& h, string& p)
+{
+	vector<int> ret;
+	vector<int> pi = getPi(p);
+	int n = h.size(), m = p.size(), j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		while (j > 0 && h[i] != p[j])
+			j = pi[j - 1];
+		if (h[i] == p[j])
+		{
+			if (j == m - 1)
+			{
+				ret.push_back(i - m + 2);
+				j = pi[j];
+			}
+			else
+				j++;
+		}
+	}
+	return ret;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	string t, p;
+	getline(cin, t);
+	getline(cin, p);
+	vector<int> ans = kmp(t, p);
+	cout << ans.size() << '\n';
+	for (int a : ans)
+		cout << a << " ";
+}
+
+// https://www.acmicpc.net/problem/1786
