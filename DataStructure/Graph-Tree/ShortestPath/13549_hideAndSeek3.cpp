bf41279835fc85542bdb628afa24c3e0a1bace73
tlsgkdns <tlsgkdns@gmail.com>
2023-06-28 04:23:23 +0900

    0-1 BFS to Dijkstra

--- DataStructure/Graph-Tree/ShortestPath/13549_hideAndSeek3.cpp
+++ DataStructure/Graph-Tree/ShortestPath/13549_hideAndSeek3.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <cstring>
-#include <deque>
+#include <queue>
+
 using namespace std;
+
 int v[200001];
 int N, K;
 int main()
 {
-	memset(v, -1, sizeof(v));
-	deque<int> dq;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	for (int i = 0; i <= 200000; i++) v[i] = 1000000007;
 	cin >> N >> K;
-	dq.push_back(N);
 	v[N] = 0;
-	while (!dq.empty())
+	priority_queue<pair<int, int>> pq;
+	pq.push({ 0, N });
+	while (!pq.empty())
 	{
-		int now = dq.front();
-		dq.pop_front();
-		if (now == K) break;
-		int next = now * 2;
-		if (next <= 200000 && v[next] == -1)
+		int cost = -pq.top().first;
+		int here = pq.top().second;
+		pq.pop();
+		if (v[here] < cost) continue;
+		if (here == K) break;
+		int there = here * 2;
+		if (there <= 200000 && v[there] > cost)
 		{
-			v[next] = v[now];
-			dq.push_front(next);
+			v[there] = cost;
+			pq.push({ -v[there], there });
 		}
-		next = now + 1;
-		if (next <= 100000 && v[next] == -1)
+		there = here + 1;
+		if (there <= 100000 && v[there] > cost + 1)
 		{
-			v[next] = v[now] + 1;
-			dq.push_back(next);
+			v[there] = cost + 1;
+			pq.push({ -v[there], there });
 		}
-		next = now - 1;
-		if (next >= 0 && v[next] == -1)
+		there = here - 1;
+		if (there >= 0 && v[there] > cost + 1)
 		{
-			v[next] = v[now] + 1;
-			dq.push_back(next);
+			v[there] = cost + 1;
+			pq.push({ -v[there], there });
 		}
-
 	}
 	cout << v[K] << '\n';
 }
