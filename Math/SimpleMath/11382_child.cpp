89ad76309ca9f9594790c45400ce565b1089ece0
tlsgkdns <tlsgkdns@gmail.com>
2024-08-19 08:58:45 +0900

    Simple Math

---
+++ Math/SimpleMath/11382_child.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long A, B, C;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> A >> B >> C;
+	cout << A + B + C << '\n';
+}
+
+// https://www.acmicpc.net/problem/11382
