f2fea5299925ba4a94976718e1d08c9825e984c4
tlsgkdns <tlsgkdns@gmail.com>
2024-12-11 18:00:40 +0900

    Solved like Eratosthenes method

---
+++ Math/Eratosthenes/17427_sumOfDivisor2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+long long ans = 0, d[1000001];
+int N;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = i; j <= N; j += i)
+			d[j] += i;
+	}
+	for (int i = 1; i <= N; i++) ans += d[i];
+	cout << ans << '\n';
+}
+
+// https://www.acmicpc.net/problem/17427
+
+/*
+solve like Eratosthenes
+*/
