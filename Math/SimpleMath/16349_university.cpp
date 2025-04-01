53f89e17c8824bc0c5b8e183d48bee786f1ebd1f
tlsgkdns <tlsgkdns@gmail.com>
2025-04-01 13:25:55 +0900

    Simple Math

---
+++ Math/SimpleMath/16349_university.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	cout << N - 1946 << '\n';
+}
+
+// https://www.acmicpc.net/problem/16394
