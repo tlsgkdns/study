4ed9962faa1d7d3b314af271e4eeac9a68fc6340
tlsgkdns <tlsgkdns@gmail.com>
2025-06-17 07:15:27 +0900

    Just Print

---
+++ Implement/5338_MicrosoftLogo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cout << "       _.-;;-._\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-;;-._|\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-''-._|\n";
+}
+
+// https://www.acmicpc.net/problem/5338
