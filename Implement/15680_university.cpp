29577f4e40f7e42ddda1ac0a93ae9761c2e693b1
tlsgkdns <tlsgkdns@gmail.com>
2025-07-05 10:02:07 +0900

    if Else

---
+++ Implement/15680_university.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	if (N == 0) cout << "YONSEI\n";
+	else cout << "Leading the Way to the Future\n";
+}
+
+// https://www.acmicpc.net/problem/15680
