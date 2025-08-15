b0e404fd710517e2d055e5e0ac0965083bc75ccc
tlsgkdns <tlsgkdns@gmail.com>
2025-08-15 14:51:31 +0900

    Simple Math

---
+++ Math/SimpleMath/24086_height.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> A >> B;
+	cout << B - A << '\n';
+}
+
+// https://www.acmicpc.net/problem/24086
