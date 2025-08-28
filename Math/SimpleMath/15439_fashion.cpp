ae2d9e004d216311e09b87ef254ea13193009131
tlsgkdns <tlsgkdns@gmail.com>
2025-08-28 10:33:06 +0900

    Simple Math

---
+++ Math/SimpleMath/15439_fashion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	cout << (N - 1) * N << '\n';
+}
+
+// https://www.acmicpc.net/problem/15439
