d41de7b18dc9541ce4176a501085cd824b95eb0b
tlsgkdns <tlsgkdns@gmail.com>
2025-08-31 09:47:40 +0900

    Copy and Paste

---
+++ Implement/25083_sprout.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+}
+
+// https://www.acmicpc.net/problem/25083
