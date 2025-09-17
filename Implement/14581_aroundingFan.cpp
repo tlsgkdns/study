5b0471747c8c1acd3740089c8001105972ca5981
tlsgkdns <tlsgkdns@gmail.com>
2025-09-17 19:55:19 +0900

    Arounding

---
+++ Implement/14581_aroundingFan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+string S;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> S;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << S << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+}
+
+// https://www.acmicpc.net/problem/14581
