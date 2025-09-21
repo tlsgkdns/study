dc2f726bacdc05706ea1bb5704d6b5ca74e5c064
tlsgkdns <tlsgkdns@gmail.com>
2025-09-22 07:39:53 +0900

    Simple Math

---
+++ Math/SimpleMath/24264_algorithmLecture-algorithm'sExecutionTime3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+long long n;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	cout << n * n << '\n' << 2 << '\n';
+}
+
+// https://www.acmicpc.net/problem/24264
