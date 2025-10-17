707db90bc7e5549207c61a58093ff3e95ec3d3bb
tlsgkdns <tlsgkdns@gmail.com>
2025-10-17 20:07:56 +0900

    Print Input

---
+++ Implement/24263_algorithm'sLecture-Algorithm'sExecutionTime2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	cout << N << "\n1\n";
+}
+
+// https://www.acmicpc.net/problem/24263
