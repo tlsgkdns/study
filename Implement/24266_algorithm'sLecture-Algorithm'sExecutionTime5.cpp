9b9aea0105ad0b153d4efa3d45a96cf7f0fca429
tlsgkdns <tlsgkdns@gmail.com>
2025-10-24 23:41:19 +0900

    Three

---
+++ Implement/24266_algorithm'sLecture-Algorithm'sExecutionTime5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+long long N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> N;
+	cout << N * N * N << '\n' << 3 << '\n';
+}
+
+// https://www.acmicpc.net/problem/24266
