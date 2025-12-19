ad426e31df47e6b963e9933a348cbaf1f0d1b126
tlsgkdns <tlsgkdns@gmail.com>
2025-12-19 20:13:54 +0900

    SciComLove

---
+++ Implement/21598_SciComLove.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+	cin >> N;
+	while (N--)
+	{
+		cout << "SciComLove\n";
+	}
+}
+
+// https://www.acmicpc.net/problem/21598
