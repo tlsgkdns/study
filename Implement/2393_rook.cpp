7e199a44f2e19e750d4674e9cd0e5d1081b47803
tlsgkdns <tlsgkdns@gmail.com>
2025-12-29 20:21:39 +0900

    Double Dollar

---
+++ Implement/2393_rook.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+	cout << "  ___  ___  ___\n";
+	cout << "  | |__| |__| |\n";
+	cout << "  |           |\n";
+	cout << "   \\_________/\n";
+	cout << "    \\_______/\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |_____|\n";
+	cout << "  __/       \\__\n";
+	cout << " /             \\\n";
+	cout << "/_______________\\\n";
+}
+
+// https://www.acmicpc.net/problem/2393
