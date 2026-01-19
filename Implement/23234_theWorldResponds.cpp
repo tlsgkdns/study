ef834172cb9232ec5687fbc060f0258514ae2fed
tlsgkdns <tlsgkdns@gmail.com>
2026-01-19 21:13:00 +0900

    Simple Print

---
+++ Implement/23234_theWorldResponds.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cout << "The world says hello!\n";
+}
+
+// https://www.acmicpc.net/problem/23234
