afbec379a59e75a8d841b127df7efaaa9f1f6e39
tlsgkdns <tlsgkdns@gmail.com>
2026-02-15 15:38:23 +0900

    Fake

---
+++ Implement/14645_verse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cout << "ºñ¿ÍÀÌ\n";
+}
+
+// https://www.acmicpc.net/problem/14645
