8453993227725953c520f748a09b6197d7555b13
tlsgkdns <tlsgkdns@gmail.com>
2026-02-16 14:13:43 +0900

    Simple Math

---
+++ Math/SimpleMath/24082_cube.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+long long X;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> X;
+	cout << X * X * X << '\n';
+}
+
+// https://www.acmicpc.net/problem/24082
